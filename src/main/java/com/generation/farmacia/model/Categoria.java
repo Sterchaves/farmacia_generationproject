3c47986f81b0bb370883a708064b3cf438c354e2
Stephanny Chaves <stefannychaves1001@gmail.com>
2023-10-19 10:09:17 -0300

    first commit

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,13 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+
+}
